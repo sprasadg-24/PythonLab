c168abb4221c5edb255f32adc61edd2d29f13a6b
Sivaprasad G <sivaprasad.g@live.com>
2024-06-18 23:05:19 +0530

    day 1 finished

---
+++ Day 1/exercise1.py
@@ -0,0 +1,3 @@
+# Write a program that calculates and outputs the number of characters in any name.
+name = input("What is your name? ")
+print(len(name))
---
+++ Day 1/print.py
@@ -0,0 +1,12 @@
+# 1. Mix 500g of Flour, 10g Yeast and 300ml Water in a bowl.
+# 2. Knead the dough for 10 minutes.
+# 3. Add 3g of Salt.
+# 4. Leave to rise for 2 hours.
+# 5. Bake at 200 degrees C for 30 minutes.
+# Write a program that uses print statements to print the following recipe into the Output console. 
+# The text to print is already there, you just need to make it into code.
+print('1. Mix 500g of Flour, 10g Yeast and 300ml Water in a bowl.')
+print('2. Knead the dough for 10 minutes.')
+print('3. Add 3g of Salt.')
+print('4. Leave to rise for 2 hours.')
+print('5. Bake at 200 degrees C for 30 minutes.')

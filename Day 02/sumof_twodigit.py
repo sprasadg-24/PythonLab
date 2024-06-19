21d3a92d46f9dd8a537caeedad150b533b3ac08e
Sivaprasad G <sivaprasad.g@live.com>
2024-06-19 16:03:56 +0530

    day02, finished

---
+++ Day 02/sumof_twodigit.py
@@ -0,0 +1,8 @@
+two_digit_number = input()
+# 🚨 Don't change the code above 👆
+####################################
+# Write your code below this line 👇
+first_num = int(two_digit_number[0])
+second_num = int(two_digit_number[1])
+sum = first_num + second_num
+print(sum) 
